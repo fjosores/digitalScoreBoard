8803301a91a8b4dfdbd24acbddb57b2e7ba7fe87
User <User@192.168.0.2>
2023-09-17 10:27:10 -0300

    Initial commit.

---
+++ digitalScoreBoard/src/test/java/ar/com/tibau/digitalScoreBoard/DigitalScoreBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.tibau.digitalScoreBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalScoreBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
